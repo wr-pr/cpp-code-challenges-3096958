f9a988e5ceab41032cd0ae1a015a7674d15f5bfc
Peter W <92974901+wr-pr@users.noreply.github.com>
2024-02-07 11:54:15 +0000

    ch7

--- src/07/Challenge/complex_num.cpp
+++ src/07/Challenge/complex_num.cpp
@@ -1,33 +1,38 @@
 #include "complex_num.h"
 
-complex_num::complex_num(double r, double i){
-    // Write your code here
+complex_num::complex_num(double r, double i):real(r),imag(i){
 }
 
 complex_num complex_num::operator +(complex_num w){
     complex_num z;
-    // Write your code here
+    z.real = real + w.real;
+    z.imag = imag + w.imag;
     return z;
 }
 
 complex_num complex_num::operator -(complex_num w){
     complex_num z;
-    // Write your code here
+    z.real = real - w.real;
+    z.imag = imag - w.imag;
     return z;
 }
 
 complex_num complex_num::operator *(complex_num w){
     complex_num z;
-    // Write your code here
+    z.real = real * w.real - imag * w.imag;
+    z.imag = real * w.imag + imag * w.real;
     return z;
 }
 
 complex_num complex_num::operator /(complex_num w){
     complex_num z;
-    // Write your code here
+    double denum = w.real*w.real + w.imag*w.imag;
+    z.real = (real * w.real + imag * w.imag)/ denum;
+    z.imag = (imag * w.real - real * w.imag)/ denum;
     return z;
 }
 
 void complex_num::print(std::ostream &os){
-    // Write your code here
+    if (imag >= 0) os << real << " + " << imag << "j";
+    else os << real << " -" << -imag << "j";
 }
--- src/07/Challenge/complex_num.h
+++ src/07/Challenge/complex_num.h
@@ -5,7 +5,8 @@
 
 class complex_num{
     
-    // Write your code here
+    double real;
+    double imag;
 
     public:
          complex_num(double = 0.0, double = 0.0);
